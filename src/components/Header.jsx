7bac46da12e1f4238606b9f80295a335fba54dfb
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-21 23:52:19 +0200

    [UPD] Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <h1>Boolean Movies</h1>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
