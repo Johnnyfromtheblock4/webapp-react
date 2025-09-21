2a7d6e70895a6be685bba22116c9b3d28d9a4df9
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-22 00:48:42 +0200

    [UPD] homepage impostazione

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="container my-5">
+      <div className="row">
+        <div className="col-12 text-center">
+          <h1>Booleanix</h1>
+          <h2>Film di qualità</h2>
+        </div>
+        <div className="col-12">{/* {Elenco dei libri} */}</div>
+      </div>
     </div>
   );
 };
