ba13c76fc503fbd699073925bd639b057f0f5837
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-22 00:40:46 +0200

    [UPD] rotte

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-
+import DefaultLayout from "./layouts/DefaultLayout";
+import Homepage from "./pages/Homepage";
+import DetailMovie from "./pages/DetailMovie";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
-
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/movie/:id" element={<DetailMovie />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
