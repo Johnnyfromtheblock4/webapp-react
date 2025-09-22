fd0e8702dfc4dc41e3c5c1f338a32e2de6f854c0
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-22 02:26:51 +0200

    [UPD] card style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
       <header className="container-fluid">
         <div className="row">
           <div className="col-12">
-            <img src="./imgs/logo.png" alt="" />
-            <h2 className={`${styles.title_style} `}>
+            <img src="./imgs/logo.png" className={`${styles.logo}`} />
+            <h2 className={`${styles.title_style}`}>
               Booleanix: <span className="fst-italic">La tua videoteca</span>
             </h2>
           </div>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,7 +8,25 @@ const Homepage = () => {
           <h1>Booleanix</h1>
           <h2>Film di qualità</h2>
         </div>
-        <div className="col-12">{/* {Elenco dei libri} */}</div>
+        <div className="col-12 col-md-6 col-lg-4">
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./public/imgs/movies_cover/the_godfather.jpg"
+              className="card-img-top"
+              alt="Movie"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Il Padrino</h5>
+              <p className="card-text">
+                Capolavoro di Francis Ford Coppolla sulle vicende della più
+                famosa famiglia malavitosa americana: i Corleone.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Dettagli
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
