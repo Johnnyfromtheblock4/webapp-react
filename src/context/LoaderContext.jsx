393862315fe20055cc6ab1bd16d0b8a5477205b7
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-24 18:14:18 +0200

    [UPD] LoaderContext.jsx

---
+++ src/context/LoaderContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState, useContext } from "react";
+import Loader from "../components/Loader";
+
+const LoaderContext = createContext();
+
+export const LoaderProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+
+  const showLoader = () => setLoading(true);
+  const hideLoader = () => setLoading(false);
+
+  return (
+    <LoaderContext.Provider value={{ showLoader, hideLoader }}>
+      {children}
+      {loading && <Loader />}
+    </LoaderContext.Provider>
+  );
+};
+
+// Custom hook per usarlo facilmente nei componenti
+export const useLoader = () => useContext(LoaderContext);
