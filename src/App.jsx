064f023e1dc9a0ddfd88e07b9b5465daae979e0a
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-24 18:16:15 +0200

    [UPD] wrap App con LoaderProvider

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,22 @@ import NotFound from "./pages/NotFound";
 import CreateMovie from "./pages/CreateMovie";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// importiamo il LoaderProvider
+import { LoaderProvider } from "./context/LoaderContext";
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/movie/:id" element={<DetailMovie />} />
-          <Route path="/movies/create" element={<CreateMovie />} />
-          <Route path="/not-found" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <LoaderProvider>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/movie/:id" element={<DetailMovie />} />
+            <Route path="/movies/create" element={<CreateMovie />} />
+            <Route path="/not-found" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </LoaderProvider>
     </BrowserRouter>
   );
 }
